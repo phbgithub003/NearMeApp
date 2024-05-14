62bd3504083478c0fdb10f1821cd45dc97d307e0
harshitp <palacheralaharshit@gmail.com>
2024-05-14 18:39:10 +0530

    adding search textfield

--- NearMeApp/NearMeApp/ViewController.swift
+++ NearMeApp/NearMeApp/ViewController.swift
@@ -16,6 +16,18 @@ class ViewController: UIViewController {
         map.translatesAutoresizingMaskIntoConstraints = false
         return map
     }()
+    
+    lazy var searchTextField:UISearchTextField = {
+       let textField = UISearchTextField()
+        textField.layer.cornerRadius = 10
+        textField.backgroundColor = .white
+        textField.placeholder = "Search"
+        textField.clipsToBounds = true
+        textField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 0))
+        textField.leftViewMode = .always
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,7 +35,14 @@ class ViewController: UIViewController {
     }
     
     private func setUpUI() {
+        view.addSubview(searchTextField)
+        searchTextField.heightAnchor.constraint(equalToConstant: 44).isActive = true
+        searchTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        searchTextField.widthAnchor.constraint(equalToConstant: view.bounds.size.width/1.2).isActive = true
+        searchTextField.topAnchor.constraint(equalTo: view.topAnchor,constant: 60).isActive = true
+        searchTextField.returnKeyType = .go
         view.addSubview(mapView)
+        view.bringSubviewToFront(searchTextField)
         mapView.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
         mapView.heightAnchor.constraint(equalTo: view.heightAnchor).isActive = true
         mapView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
